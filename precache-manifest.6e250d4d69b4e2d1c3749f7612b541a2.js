d0fc767c2eb143cc42541c00fafdb9a097cd9727
siripaha <siripapha.ch.61@ubu.ac.th>
2020-09-02 12:32:35 +0700

    Updates

---
+++ precache-manifest.6e250d4d69b4e2d1c3749f7612b541a2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59e764240fb16afaab8c18328912e389",
+    "url": "/-project-tic-tac-toe/index.html"
+  },
+  {
+    "revision": "b07a0865b0f2ef4059f8",
+    "url": "/-project-tic-tac-toe/static/css/main.62d0cebd.chunk.css"
+  },
+  {
+    "revision": "642f11ce68d236595a80",
+    "url": "/-project-tic-tac-toe/static/js/2.a2c56392.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/-project-tic-tac-toe/static/js/2.a2c56392.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b07a0865b0f2ef4059f8",
+    "url": "/-project-tic-tac-toe/static/js/main.dc52adef.chunk.js"
+  },
+  {
+    "revision": "ee49f13bc8aa24c0352b",
+    "url": "/-project-tic-tac-toe/static/js/runtime-main.d140c6e1.js"
+  }
+]);
